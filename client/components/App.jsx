a780dfbda192978801f761c9ea6b68d00b73fd47
Claire2888 <70086563+Claire2888@users.noreply.github.com>
2020-12-03 17:25:33 +1300

    Set up front end components

---
+++ client/components/App.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+// import in other components
+
+// const or class
+// const App = () => {
+// <div>
+// </div>
+// }
+
+// render () {
+// return
+//
+// <header>
+//   <h1 className="title"><Link to="/">Garde<span>nz</span></Link></h1>
+//   <Route path="/" component={Nav} />
+// </header>
+// <div className="columns">
+//   <Route exact path='/' component={Home} />
+// </div> /
+// }
+
+// export default App
---
+++ client/components/Home.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import {  } from 'react-router-dom'
+
+// import Map from './Map'
+
+// function Home () {
+//   return (
+//     <>
+//       <div className='column is-half-tablet'>
+//         <p>or government subsidies</p>
+//         <Link className="" to= /Link>
+//       </div>
+//       <Map/>
+//     </>
+//   )
+// }
+// export default Home
