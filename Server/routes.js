bcba824a52fbad656c4ef1c034e69d9e530375d3
maria-gedye <mariagedye@gmail.com>
2020-12-04 10:56:54 +1300

    get and post routes added

---
+++ Server/routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const db = require('./db')
+const route = express.Router()
+
+route.use(express.json())
+
+route.get('/', (req, res) => {
+  db.getPins()
+    .then((response) => {
+      return res.status(200).json(response)
+    })
+    .catch(err => {
+      res.status(500).send('DB error' + err)
+    })
+})
+
+route.post('/', (req, res) => {
+  db.addPin(req.body)
+    .then((response) => {
+      return res.status(201).json(response)
+    })
+    .catch(err => {
+      res.status(500).send('DB error' + err)
+    })
+})
+
+module.exports = route
