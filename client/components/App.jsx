6de94722143b6e2a05f44da49cb5b50fa72a6aa1
maria-gedye <mariagedye@gmail.com>
2020-12-04 14:34:59 +1300

    components in process

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
-// import Header from './Header'
+import Header from './Header'
 // import Home from './Home'
 import { Route } from 'react-router-dom'
 
 // const or class
 const App = () => (
   <div>
-    <h1>Yelllooooo </h1>
+    <Route path='/' render={() => { return <Header/> }}/>
   </div>
 )
 
