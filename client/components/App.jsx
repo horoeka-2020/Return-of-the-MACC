f090b11af6baa8cccc812d96c47f18041047c215
caroline-pitcher <70181184+caroline-pitcher@users.noreply.github.com>
2020-12-04 15:17:32 +1300

    App and header component are working but backend and frontend not connected yet

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
 import Header from './Header'
 // import Home from './Home'
-import { Route } from 'react-router-dom'
+// import { Route } from 'react-router-dom'
 
 // const or class
 const App = () => (
   <div>
-    <Route path='/' render={() => { return <Header/> }}/>
+  <Header/> 
+    {/* <Route path='/' render={() => { return <Header/> }}/> */}
   </div>
 )
 
-export default connect()(App)
+export default App
+
+// export default connect()(App)
