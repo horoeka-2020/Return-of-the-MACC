33ccb8856d86228e374e54ccd46fd4ebdd3e84e9
maria-gedye <mariagedye@gmail.com>
2020-12-05 16:24:27 +1300

    db updated to 2 tables

--- server/db/seeds/pins.js
+++ server/db/seeds/pins.js
@@ -1,27 +1,13 @@
-exports.seed = (knex) => {
-    // Deletes ALL existing entries
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
   return knex('pins').del()
-    .then(() => {
+    .then(function () {
       // Inserts seed entries
-      return knex('pins').insert([{
-        id: 1,
-        song_title: 'Here comes the sun',
-        song_artist: 'The Beatles',
-        song_album: 'Abbey Road',
-        username: 'Caz',
-        lat: '',
-        lon: '',
-        time_date: ''
-      },
-      {
-        id: 2,
-        song_title: 'Hey Jude',
-        song_artist: 'The Beatles',
-        song_album: 'The White Album',
-        username: 'Anna',
-        lat: '',
-        lon: '',
-        time_date: ''
-      }])
-    })
-}
+      return knex('pins').insert([
+        {id: 1, image: 'images/marker-yellow.png', lat: 1425, lng: -1335},
+        {id: 2, image: 'images/marker-blue.png', lat: 1425, lng: -1335},
+        {id: 3, image: 'images/marker-red.png', lat: 1425, lng: -1335}
+      ]);
+    });
+};
