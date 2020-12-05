7e66c047a10149a9e26826849b6d2db4fef136bb
Claire2888 <70086563+Claire2888@users.noreply.github.com>
2020-12-05 18:36:50 +1300

    edit scss styling

---
+++ client/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer () {
+  return (
+    <footer className='container m-5 is-round'>
+    <div className='block'>
+      <h1 className='title is-1'>Add a pin</h1>
+    </div>
+  </footer> 
+  )
+}
+
+export default Footer
