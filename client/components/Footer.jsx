07ac2fa19dd2358b1c9c8edc269648030f5ef411
caroline-pitcher <70181184+caroline-pitcher@users.noreply.github.com>
2020-12-06 15:31:08 +1300

    Added a link for add a pin to footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer () {
   return (
     <footer className='container m-5 is-round'>
     <div className='block'>
-      <h1 className='title is-1'>Add a pin</h1>
+      <h1 className='title is-1'><Link to='/form'>Add a pin</Link></h1>
     </div>
   </footer> 
   )
