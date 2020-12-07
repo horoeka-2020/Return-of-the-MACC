127ff0631746dabb967f9e3d5c9e2534cea2a5d2
caroline-pitcher <70181184+caroline-pitcher@users.noreply.github.com>
2020-12-07 19:47:57 +1300

    Changed map to satellite instead of regular

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -5,10 +5,10 @@ function Map (props) {
     return (
         <MapContainer
           center={[-36.8666700, 174.7666700]}
-          zoom={16}
+          zoom={12}
           scrollWheelZoom={true}>
           <TileLayer
-            url='https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbC1yIiwiYSI6ImNrZXM1Zm9iaDJiNmYycW1za2dobDZ4d3gifQ.a5mK2DxNqWhlzvoa8Zxb2Q'/>
+            url='https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWljaGFlbC1yIiwiYSI6ImNrZXM1Zm9iaDJiNmYycW1za2dobDZ4d3gifQ.a5mK2DxNqWhlzvoa8Zxb2Q'/>
           
           {props.pinsCoordinates?.map((location) => {
             console.log(location)
