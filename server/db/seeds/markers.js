7dfb712a0ceac079f726c7f2b4b09a81b4d3efca
caroline-pitcher <70181184+caroline-pitcher@users.noreply.github.com>
2020-12-09 18:43:28 +1300

    Updated seeds lat and lng to strings

--- server/db/seeds/markers.js
+++ server/db/seeds/markers.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('markers').insert([
-        {id: 1, song: 'Return of the Mack', artist: 'Mark Morrison', album: 'Ladies of the Canyon', username: 'Maria', spotify_link: 'https://open.spotify.com/embed/track/3jDdpx9PMlfMBS5tOBHFm9', lat: -36.8445085, lng: 174.7659743},
-        {id: 2, song: 'Ive had the time of my life', artist: 'Various Artists', album: 'Smile', username: 'Caroline', spotify_link: 'https://open.spotify.com/embed/track/6W7ztLBiRzBN46ZaPAcQ0F', lat: -36.8558754, lng: 174.7713387},
-        {id: 3, song: 'Bye Bye Bye', artist: 'Nsync', album: 'No string', username: 'Anna', spotify_link: 'https://open.spotify.com/embed/track/4r8lRYnoOGdEi6YyI5OC1o', lat: -36.8706052, lng: 174.7797501},
+        {id: 1, song: 'Return of the Mack', artist: 'Mark Morrison', album: 'Ladies of the Canyon', username: 'Maria', spotify_link: 'https://open.spotify.com/embed/track/3jDdpx9PMlfMBS5tOBHFm9', lat: '-36.8445085', lng: '174.7659743'},
+        {id: 2, song: 'Ive had the time of my life', artist: 'Various Artists', album: 'Smile', username: 'Caroline', spotify_link: 'https://open.spotify.com/embed/track/6W7ztLBiRzBN46ZaPAcQ0F', lat: '-36.8558754', lng: '174.7713387'},
+        {id: 3, song: 'Bye Bye Bye', artist: 'Nsync', album: 'No string', username: 'Anna', spotify_link: 'https://open.spotify.com/embed/track/4r8lRYnoOGdEi6YyI5OC1o', lat: '-36.8706052', lng: '174.7797501'},
         // final song to be added for the demo
         // {id: 4, song: 'Graduation Friends forever', artist: 'Vitamin C', album: 'One hit wonder', username: 'Claire', spotify_link: 'https://open.spotify.com/embed/track/45V4Mk7vZV2b9JfSWuUX9m'
       ])
